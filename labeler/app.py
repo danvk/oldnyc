ce56ea95418f006209a945c93d568a424c20bb61
Dan Vanderkam <dan@dygraphs.com>
2011-02-20 15:21:35 -0500

    cookie-assigning system

--- labeler/app.py
+++ labeler/app.py
@@ -1,6 +1,40 @@
 import db
+import cgi
 
-print 'Content-Type: text/plain'
-print ''
-print 'Hello, world!'
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class MainPage(webapp.RequestHandler):
+  def get(self):
+    cookie = None
+    if 'id' in self.request.cookies:
+      cookie = self.request.cookies['id']
+
+    user = None
+    if not cookie:
+      # Create a new entry for this user and set their cookie.
+      user = db.User()
+      user.put()
+      self.response.headers.add_header(
+        'Set-Cookie',
+        'id=%s' % user.key(),
+        Expires='Wed, 13-Jan-2021 22:23:01 GMT')
+
+    else:
+      # Get their record.
+      user = db.User.get(cookie)
+      assert user
+
+    self.response.out.write('<html><body>uid: %s</boby></html>' % user.key())
+
+
+application = webapp.WSGIApplication(
+                                     [('/', MainPage)],
+                                     debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
 
