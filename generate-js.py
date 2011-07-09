be6d46e54bdd417f1249ee48e7a1a799454d8f9a
Dan Vanderkam <dan@dygraphs.com>
2011-07-09 18:44:55 -0400

    show numbered dots based on # of photos in a location

--- generate-js.py
+++ generate-js.py
@@ -4,6 +4,7 @@
 
 import re
 import record
+from collections import defaultdict
 
 # build a photo_id -> Record dict
 rs = record.AllRecords()
@@ -13,6 +14,10 @@ for r in rs:
 
 # load the list of geocodes
 lines = file('/tmp/pair-geocodes.txt').read().split('\n')
+
+# "lat,lon" -> list of photo_ids
+ll_to_id = defaultdict(list)
+
 codes = []
 for line in lines:
   if not line: continue
@@ -21,21 +26,25 @@ for line in lines:
   photo_id = line.split("\t")[0]
   lat_lon = line.split("\t")[1]
   lat, lon = [float(x) for x in lat_lon.split(",")]
-  codes.append((photo_id, lat, lon))
+  ll_to_id['%f,%f' % (lat, lon)].append(photo_id)
 
-print "var lat_lons = ["
-for photo_id, lat, lon in codes:
-  r = id_to_record[photo_id]
-  date_range = r.date_range()
-  # saves ~9k. could save more by doing '0.' -> '.' or ''
-  # lat -= 37
-  # lon += 123
-  if date_range and date_range[0] and date_range[1]:
-    # TODO(danvk): use a more compact date format.
-    print '[%f,%f,"%d/%d/%d","%d/%d/%d","%s"],' % (
-      lat, lon,
-      date_range[0].year, date_range[0].month, date_range[0].day,
-      date_range[1].year, date_range[1].month, date_range[1].day,
-      r.photo_id())
+print "var lat_lons = {"
+for lat_lon, photo_ids in ll_to_id.iteritems():
+  recs = []
+  for photo_id in photo_ids:
+    r = id_to_record[photo_id]
+    date_range = r.date_range()
+    # saves ~9k. could save more by doing '0.' -> '.' or ''
+    # lat -= 37
+    # lon += 123
+    if date_range and date_range[0] and date_range[1]:
+      # TODO(danvk): use a more compact date format.
+      recs.append('["%d/%d/%d","%d/%d/%d","%s"]' % (
+        date_range[0].year, date_range[0].month, date_range[0].day,
+        date_range[1].year, date_range[1].month, date_range[1].day,
+        r.photo_id()))
+    
+  if recs:
+    print '"%s": [%s],' % (lat_lon, ','.join(recs))
 
-print "];"
+print "};"
