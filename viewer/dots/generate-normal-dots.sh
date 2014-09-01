55309df2a69d97a5553bb414bf42c2c2519cee0b
Dan Vanderkam <danvdk@gmail.com>
2014-08-31 23:31:36 -0400

    darken dots a bit (196 --> 160 min)

--- viewer/dots/generate-normal-dots.sh
+++ viewer/dots/generate-normal-dots.sh
@@ -6,10 +6,10 @@ stroke_color='#111111'
 text_color='#111111'
 font=Helvetica-Bold
 
-convert -size 9x9 xc:none -fill 'rgb(196,196,196)' -draw 'circle 4,4 7,7' 1.png
+convert -size 9x9 xc:none -fill 'rgb(160,160,160)' -draw 'circle 4,4 7,7' 1.png
 
 for x in $(seq 2 100); do
-  shade=$( echo "floor(196 - 196 * $x/100)" | bc)
+  shade=$( echo "floor(160 - 160 * $x/100)" | bc)
   rgb="rgb($shade,$shade,$shade)"
   convert -size 13x13 xc:none -fill $rgb -draw 'circle 6,6 10,10' $x.png
 done
