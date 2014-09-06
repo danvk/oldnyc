17995ca04c19ee86322c32aa633af1b0b60fa213
Dan Vanderkam <danvdk@gmail.com>
2014-09-06 15:10:57 -0400

    mass OldSF deletion

--- blacklist.lat-lons.js
+++
@@ -1,4 +0,0 @@
-# These lat/lons are manually removed because they're embarrassing false positives.
-# These two are incorrectly placed on Treasure Island:
-37.829840,-122.368532
-37.818938,-122.372709
--- labeler/console/__init__.py
+++
@@ -1,1 +0,0 @@
-"""App Engine Console package"""
