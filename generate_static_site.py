afdd350ba376b027e04799fb3eef119d539e8d29
Dan Vanderkam <danvdk@gmail.com>
2015-03-15 18:39:41 -0400

    fix file name typo

--- generate_static_site.py
+++ generate_static_site.py
@@ -2,10 +2,15 @@
 '''Generate a static version of oldnyc.org consisting entirely of JSON.'''
 
 import chardet
+from collections import defaultdict
 import csv
 import json
 import record
 import re
+
+# strip leading 'var popular_photos = ' and trailing ';'
+popular_photos = json.loads(open('viewer/static/js/popular-photos.js', 'rb').read()[20:-2])
+pop_ids = {x['id'] for x in popular_photos}
 
 # strip leading 'var lat_lons = ' and trailing ';'
 lat_lon_to_ids = json.loads(open('viewer/static/js/nyc-lat-lons-ny.js', 'rb').read()[15:-1])
@@ -33,9 +38,8 @@ def decode(b):
     except UnicodeDecodeError:
         return b.decode(chardet.detect(b)['encoding'])
 
-latlon_to_count = {}
-for latlon, photo_ids in lat_lon_to_ids.iteritems():
-    outfile = '../oldnyc.github.io/by-location/%s.json' % latlon.replace(',', '')
+
+def make_response(photo_ids):
     response = {}
     for photo_id in photo_ids:
         r = id_to_record[photo_id]
@@ -56,9 +60,27 @@ for latlon, photo_ids in lat_lon_to_ids.iteritems():
           'height': h,
           'text': ocr_text
         }
+    return response
 
+
+latlon_to_count = {}
+id4_to_latlon = defaultdict(lambda: {})  # first 4 of id -> id -> latlon
+for latlon, photo_ids in lat_lon_to_ids.iteritems():
+    outfile = '../oldnyc.github.io/by-location/%s.json' % latlon.replace(',', '')
+    response = make_response(photo_ids)
     latlon_to_count[latlon] = len(response)
     json.dump(response, open(outfile, 'wb'), indent=2)
+    for id_ in photo_ids:
+        id4_to_latlon[id_[:4]][id_] = latlon
+
+json.dump(make_response(pop_ids),
+          open('../oldnyc.github.io/popular.json', 'wb'), indent=2)
 
 with open('../oldnyc.github.io/lat-lon-counts.js', 'wb') as f:
     f.write('var lat_lons = %s;' % json.dumps(latlon_to_count, indent=2))
+
+
+for id4, id_to_latlon in id4_to_latlon.iteritems():
+    json.dump(id_to_latlon,
+              open('../oldnyc.github.io/id4-to-location/%s.json' % id4, 'wb'),
+              indent=2)
