140263e0d8a6319ebfd6c35e6133e147d942b9ac
salah ahmed <salah.ahmed.hslc@gmail.com>
2016-12-22 14:34:22 -0500

    use PIL for image

--- extract-sizes.py
+++ extract-sizes.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python
-#
-# Determine the sizes of a bunch of images.
-#
-# Usage:
-# ./extract-sizes.py '*.jpg' > sizes.txt
-#
-# Produces a CSV file with three columns:
-# file-basename-no-extension,width,height
+'''
+Determine the sizes of a bunch of images.
+
+Usage:
+./extract-sizes.py '*.jpg' > sizes.txt
+
+Produces a CSV file with three columns:
+file-basename-no-extension,width,height
+'''
 
 import glob
 import os.path
-import re
-import subprocess
 import sys
+from PIL import Image
 
-for pattern in sys.argv[1:]:
-  for path in glob.glob(pattern):
-    size_str = subprocess.check_output(['identify', path])
 
-    m = re.search(r' (\d+)x(\d+) ', size_str)
-    assert m, size_str
-    width, height = [int(x) for x in m.groups()]
-    assert width > 0
-    assert height > 0
+def image_size(path):
+    image = Image.open(path)
+    width, height = image.size
+    return (width, height)
 
-    base, _ = os.path.splitext(os.path.basename(path))
-
-    print '%s,%d,%d' % (base, width, height)
+if __name__ == '__main__':
+    for pattern in sys.argv[1:]:
+        for path in glob.glob(pattern):
+            width, height = image_size(path)
+            base, _ = os.path.splitext(os.path.basename(path))
+            print('%s,%d,%d' % (base, width, height))
