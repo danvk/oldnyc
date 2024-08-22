7e521a72f418f35cac9e3518f80b024889d303a0
Dan Vanderkam <danvdk@gmail.com>
2024-08-22 11:30:59 -0400

    pytest for dates

--- dates_test.py
+++ dates_test.py
@@ -1,12 +1,10 @@
-from nose.tools import *
-
 from dates import extract_years
 
 def test_extract_dates():
-    eq_(['1927'], extract_years('1927'))
-    eq_(['1927'], extract_years('ca. 1927'))
-    eq_(['1927'], extract_years('1927?'))
-    eq_(['1932', '1933'], extract_years('1932; 1933'))
-    eq_(['1927'], extract_years('Jan. 8, 1927'))
-    eq_([''], extract_years('n.d.'))
-    eq_(['1925', '1931'], extract_years('[1925-1931]'))
+    assert extract_years('1927') == ['1927']
+    assert extract_years('ca. 1927') == ['1927']
+    assert extract_years('1927?') == ['1927']
+    assert extract_years('1932; 1933') == ['1932', '1933']
+    assert extract_years('Jan. 8, 1927') == ['1927']
+    assert extract_years('n.d.') == ['']
+    assert extract_years('[1925-1931]') == ['1925', '1931']
