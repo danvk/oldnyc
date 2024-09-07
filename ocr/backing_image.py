011ff791dfbb1e8944db0aeacf82621b6e579bbe
Dan Vanderkam <danvdk@gmail.com>
2024-09-07 13:24:17 -0400

    checkpoint backing_image.py; needs an update if still useful

--- ocr/backing_image.py
+++ ocr/backing_image.py
@@ -8,8 +8,13 @@ import record
 
 
 def photo_id_to_backing_id(photo_id: str) -> str:
-    return re.sub(r'f?(?:-[a-z])?$', 'b', photo_id, count=1)
-
+    if 'f' in photo_id:
+        return re.sub(r'f?(?:-[a-z])?$', 'b', photo_id, count=1)
+    elif re.match(r'\d+$', photo_id):
+        front = int(photo_id)
+        back = front + 1
+        return str(back)
+    return None
 
 def strip_photo_suffix(photo_id: str) -> str:
     return re.sub(r'-[a-z]*', '', photo_id)
@@ -20,26 +25,32 @@ if __name__ == '__main__':
     all_photo_ids = {r['id'] for r in rs}
 
     site_data = json.load(open('../oldnyc.github.io/data.json'))
-    photo_ids_with_text = {
-        strip_photo_suffix(photo['photo_id'])
-        for photo in site_data['photos']
-        if photo['text']
-    }
+    photo_ids = [photo['photo_id'] for photo in site_data['photos']]
+    # photo_ids_with_text = {
+    #     strip_photo_suffix(photo['photo_id'])
+    #     for photo in site_data['photos']
+    #     if photo['text']
+    # }
 
-    photo_ids = all_photo_ids.difference(photo_ids_with_text)
+    # photo_ids = all_photo_ids.difference(photo_ids_with_text)
+    # photo_ids = photo_ids_with_text
 
-    for photo_id in photo_ids:
+    out = {}
+    for in_photo_id in photo_ids:
         back_id = None
+        photo_id = in_photo_id.split('-')[0]
         if 'f' in photo_id:
             back_id = photo_id_to_backing_id(photo_id)
         elif re.match(r'\d+$', photo_id):
-            front = int(photo_id)
-            back = front + 1
-            if str(back) not in all_photo_ids:
-                back_id = str(back)
+            back = photo_id_to_backing_id(photo_id)
+            if back not in all_photo_ids:
+                back_id = back
         else:
             sys.stderr.write(f'Weird ID: {photo_id}\n')
 
-        if back_id:
-            url = f'http://images.nypl.org/?id={back_id}&t=w'
-            print('ocr/images/%s.jpg\t%s' % (back_id, url))
+        out[in_photo_id] = back_id
+        # if back_id:
+        #     url = f'http://images.nypl.org/?id={back_id}&t=w'
+        #     print('ocr/images/%s.jpg\t%s' % (back_id, url))
+
+    print(json.dumps(out))
