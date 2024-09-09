b6eda89db9430faf9277cf055188b27f78dc4403
Dan Vanderkam <danvdk@gmail.com>
2024-09-09 11:16:39 -0400

    patch GPT queries

--- geogpt/extract_gpt_geocodes.py
+++ geogpt/extract_gpt_geocodes.py
@@ -2,13 +2,29 @@
 """Extract photo ID -> geocodable string mapping from GPT JSONL output files."""
 
 import json
+import re
 import sys
+
 
 
 IGNORE_GEOCODES = {
     'not in NYC',
     'no location information',
 }
+
+
+def patch_query(q: str) -> str:
+    """GPT makes some systematic mistakes, for example combining addresses and crosses.
+
+    This fixes that.
+        4514 16th Avenue & 46th Street -> 16th Avenue & 46th Street
+        131-135 Pitt St & E Houston St -> Pitt St & E Houston St
+        334-336 Atlantic Avenue -> 336 Atlantic Avenue
+    """
+    q = re.sub(r'^\d+-(\d+) ', r'\1 ', q)
+    if '&' in q:
+        q = re.sub(r'^(\d+) ', '', q)
+    return q
 
 
 if __name__ == '__main__':
@@ -26,6 +42,6 @@ if __name__ == '__main__':
             gpt_location = data['location']
             if gpt_location in IGNORE_GEOCODES:
                 continue
-            out[id] = gpt_location
+            out[id] = patch_query(gpt_location)
 
     print(json.dumps(out, indent=2, sort_keys=True))
