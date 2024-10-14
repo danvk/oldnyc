c66b489c4c1a5cdf4a85ecbbd2ea6e19dac1b61d
Dan Vanderkam <danvdk@gmail.com>
2024-10-14 10:31:37 -0400

    all tests -> pytest, clear out ocr/tess dir

--- grid/coder_test.py
+++ grid/coder_test.py
@@ -13,13 +13,16 @@ def test_exact():
     # It's really Park Avenue South.
     assert_close(coder.code('4', '17'), (40.736518, -73.988962))
 
+
 def test_interpolate():
     # This is halfway between 26th & 28th.
     assert_close(coder.code('9', '27'), (40.749020, -73.9995210))
 
+
 def test_extrapolate():
     assert_close(coder.code('A', '15'), (40.731083, -73.979847))
     assert_close(coder.code('A', '20'), (40.734071, -73.977654))
+
 
 def test_may_extrapolate():
     assert(coder.may_extrapolate('A', '15'))
--- title_cleaner_test.py
+++ title_cleaner_test.py
@@ -1,5 +1,3 @@
-from nose.tools import *
-
 import title_cleaner
 
 TRUTH = [
