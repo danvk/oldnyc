aa2fd2fd199fed53bf2d12ed7f80ac0067c5373b
Dan Vanderkam <danvdk@gmail.com>
2024-10-16 10:31:15 -0400

    organize imports on save

--- data/ingest.py
+++ data/ingest.py
@@ -10,13 +10,7 @@ from collections import Counter
 from tqdm import tqdm
 
 from data.item import Item, Subject
-from data.util import (
-    STATES,
-    clean_creator,
-    clean_date,
-    clean_title,
-    normalize_whitespace,
-)
+from data.util import STATES, clean_creator, clean_date, clean_title, normalize_whitespace
 
 
 def photo_id_to_backing_id(photo_id: str) -> str:
