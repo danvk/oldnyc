ec2fd7bf7122f9281a5725d84bb3d7453faf38b0
Dan Vanderkam <danvdk@gmail.com>
2024-10-29 17:02:00 -0400

    sort truth data for diffing

--- oldnyc/geocode/truth/generate_truth_gtjson.py
+++ oldnyc/geocode/truth/generate_truth_gtjson.py
@@ -82,7 +82,9 @@ if __name__ == "__main__":
             )
         )
 
-    fc = pygeojson.FeatureCollection(localturk_features + surveyor_features)
+    all_features = localturk_features + surveyor_features
+    all_features.sort(key=lambda f: str(f.id))
+    fc = pygeojson.FeatureCollection(all_features)
     # fc = pygeojson.FeatureCollection(surveyor_features)
     # workaround for https://github.com/hawkaa/pygeojson/issues/18
     out = json.loads(pygeojson.dumps(fc))
