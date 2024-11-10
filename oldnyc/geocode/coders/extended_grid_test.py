c5f8c064fadd63b55fb693f111b3af8c8304ba40
Dan Vanderkam <danvdk@gmail.com>
2024-11-10 11:10:14 -0500

    add test for 464848

--- oldnyc/geocode/coders/extended_grid_test.py
+++ oldnyc/geocode/coders/extended_grid_test.py
@@ -8,3 +8,13 @@ def test_parse_street_ave():
     assert parse_street_ave("18th Street (West)", "4th Avenue") == ("4", "18")
     # 711722f
     assert parse_street_ave("9th Avenue", "23rd Street (West)") == ("9", "23")
+
+    # 464848
+    "Stanley Court, corner West End Avenue and 106th Street., Manhattan, NY"
+    # ('Stanley Court, corner West End Avenue', '106th Street.', 'Manhattan, NY')
+
+    assert parse_street_ave("West End Avenue", "106th Street") == ("11", "106")
+    assert parse_street_ave("Stanley Court, corner West End Avenue", "106th Street") == (
+        "11",
+        "106",
+    )
