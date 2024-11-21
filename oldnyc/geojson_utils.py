255da44f0212d30ec94af5153e0cefb0ddb6d666
Dan Vanderkam <danvdk@gmail.com>
2024-11-21 16:03:31 -0500

    rv geojson_utils change

--- oldnyc/geojson_utils.py
+++ oldnyc/geojson_utils.py
@@ -5,9 +5,3 @@ def assert_point(g: pygeojson.GeometryObject | None) -> pygeojson.Point:
     if not isinstance(g, pygeojson.Point):
         raise ValueError(f"Expected Point, got {g}")
     return g
-
-
-def assert_polygon(g: pygeojson.GeometryObject | None) -> pygeojson.Polygon:
-    if not isinstance(g, pygeojson.Polygon):
-        raise ValueError(f"Expected Point, got {g}")
-    return g
